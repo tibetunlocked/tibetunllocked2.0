37c18990bbe090cdae61629b40d68fce8e2494be
tibetunlocked <tibetaction21@gmail.com>
2024-09-12 14:27:01 +0530

    removed netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import NetlifyCMS from "astro-netlify-cms";
-import dcapConfig from "./decap.config.mjs";
+
 import astropodConfig from "./.astropod/astropod.config.json";
-import robotsTxt from "astro-robots-txt";
+
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -14,15 +13,6 @@ import image from "@astrojs/image";
 export default defineConfig({
   site: astropodConfig.site,
   integrations: [
-    robotsTxt({
-      policy: [
-        {
-          userAgent: "*",
-          allow: "/",
-          disallow: "/admin",
-        },
-      ],
-    }),
     mdx(),
     sitemap(),
     tailwind(),
@@ -31,8 +21,6 @@ export default defineConfig({
       cacheDir: "./.cache/image",
       logLevel: "debug",
     }),
-    NetlifyCMS({
-      config: dcapConfig(),
-    }),
+
   ],
 });
